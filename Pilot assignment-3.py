e68dddb9e1172f6c47e1052937fe5a358f34a2f7
mahesh671 <68265140+mahesh671@users.noreply.github.com>
2021-07-04 17:50:07 +0530

    Create Pilot assignment-3.py

---
+++ Pilot assignment-3.py
@@ -0,0 +1,9 @@
+height=int(input("Enter present height of plane: "))
+if height<=1000:
+  print("Safe to land")
+elif height<=5000:
+  print("Bring down to 1000")
+else:
+  print("Turn around")
+  
+  
